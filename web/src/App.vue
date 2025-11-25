97f0dd91d374683ae53c123e44d826bdda1b12ab
Florian Hoss <mail@florianhoss.de>
2025-11-25 06:55:53 +0100

    Add Question component and improve question rendering
    Introduced a new Question.vue component to encapsulate question and answer display logic. Updated App.vue to use the new component and improved loading and error states for fetching questions.

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import { getQuestions } from './client/sdk.gen';
 import type { QuestionAndAnswer } from './client/types.gen';
+import Question from './components/Question.vue';
 
 const questions = ref<QuestionAndAnswer[] | null | undefined>(null);
 getQuestions().then((resp) => {
@@ -11,9 +12,12 @@ getQuestions().then((resp) => {
 
 <template>
   <div class="container">
-    <div v-for="question in questions" :key="question.id">
-      <h3>{{ question.question }}</h3>
-      <p>{{ question.answers }}</p>
+    <div v-if="questions === null">Loading...</div>
+    <div v-else-if="questions === undefined">Error loading questions.</div>
+    <div v-else>
+      <div v-for="question in questions" :key="question.id" class="mb-4">
+        <Question :question="question" />
+      </div>
     </div>
   </div>
 </template>
---
+++ web/src/components/Question.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { QuestionAndAnswer } from '../client/types.gen';
+
+defineProps<{ question: QuestionAndAnswer }>();
+</script>
+
+<template>
+  <div class="flex items-center">
+    <h3>{{ question.question }}</h3>
+
+    <div class="flex items-center" v-for="answer in question.answers" :key="answer">
+      <input type="radio" :id="answer" :value="answer" class="radio disabled:opacity-100" />
+      <label :for="answer" class="pl-4 w-full select-none">
+        {{ answer }}
+      </label>
+    </div>
+  </div>
+</template>
